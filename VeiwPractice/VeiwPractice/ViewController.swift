f67e6f03c602472922868b58483789aca71e7fc7
sangheonlee <sanghun0724@naver.com>
2020-09-14 17:01:51 +0900

    MyPlayground.playground

---
+++ VeiwPractice/VeiwPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VeiwPractice
+//
+//  Created by sangheon on 2020/09/14.
+//  Copyright © 2020 sangheon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
