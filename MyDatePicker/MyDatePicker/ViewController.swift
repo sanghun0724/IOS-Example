38e109c0808f9d42589375693399b4e06e9d256e
sangheonlee <sanghun0724@naver.com>
2020-09-21 12:35:07 +0900

    g

--- MyDatePicker/MyDatePicker/ViewController.swift
+++ MyDatePicker/MyDatePicker/ViewController.swift
@@ -8,11 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var datePicker:UIDatePicker!
+    @IBOutlet weak var dateLabel:UILabel!
+    let dateFormatter: DateFormatter = {
+        let formatter: DateFormatter = DateFormatter()
+        formatter.dateStyle = .medium
+        formatter.timeStyle = .medium
+        //자기맘대로 formatter.dateFormat =  "yyyy/MM/dd hh:mm:ss"
+        return formatter
+    }()
+    @IBAction func didDatePickerValueChanged(_ sender:UIDatePicker){ //sender은 메소드를호출한친구
+        print("value change")
+        
+        let date:Date = self.datePicker.date
+        let dateString: String = self.dateFormatter.string(from: date)
+        
+        self.dateLabel.text = dateString
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.datePicker.addTarget(self, action: #selector(self.didDatePickerValueChanged(_:)), for: UIControl.Event.valueChanged)  // 위에 인터페이스 빌더 대신 코드로 밸류체잉지 감지
     }
+    
+    
 
 
 }
---
+++ TapGesure/TapGesure/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  TapGesure
+//
+//  Created by sangheon on 2020/09/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController , UIGestureRecognizerDelegate{
+
+    @IBAction func tapView(_ sender:UITapGestureRecognizer){
+        self.view.endEditing(true)
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+//        let tapGesture:UITapGestureRecognizer =
+//            UITapGestureRecognizer(target: self, action: #selector(self.tapView(_:)))
+//
+//        self.view.addGestureRecognizer(tapGesture)   //   코드로 제스쳐 구현
+//
+        let tapGesture: UITapGestureRecognizer = UITapGestureRecognizer()
+        tapGesture.delegate = self
+        
+        self.view.addGestureRecognizer(tapGesture)
+    }
+//밑은 딜리게이트 활용
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldReceive touch: UITouch) -> Bool {
+        //이 메소드에서 bool 부분은 차피  트루하든 폴스하든 물어본순간 사용자가 클릭! 액션한거니깐 무상관
+        self.view.endEditing(true)
+        return true
+    }
+    
+
+}
+
