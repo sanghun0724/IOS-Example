f38820968f5b85bcd4b70e8ce1eae7438ff4a970
sangheonlee <sanghun0724@naver.com>
2020-09-23 20:14:41 +0900

    go

--- MyLoginService/MyLoginService/SecondView.swift
+++ MyLoginService/MyLoginService/SecondView.swift
@@ -31,7 +31,9 @@ class SecondView: UIViewController,UIImagePickerControllerDelegate ,UINavigation
     
     
     
-    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
     
     @IBAction func nextButton(_ sender: UIButton) {
         
