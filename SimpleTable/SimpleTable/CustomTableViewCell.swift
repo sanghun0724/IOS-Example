b1d2e6ca88c63c28eb4061fb022dbbbd21d51bb6
sangheonlee <sanghun0724@naver.com>
2020-09-28 23:17:32 +0900

    y

---
+++ SimpleTable/SimpleTable/CustomTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  CustomTableViewCell.swift
+//  SimpleTable
+//
+//  Created by sangheon on 2020/09/28.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet var 
+    
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
