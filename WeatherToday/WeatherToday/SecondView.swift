0df120624d108f1306419ce03cee6c3addd19f73
sangheonlee <sanghun0724@naver.com>
2020-09-30 21:14:05 +0900

    kannye west is best

---
+++ WeatherToday/WeatherToday/SecondView.swift
@@ -0,0 +1,8 @@
+//
+//  SecondView.swift
+//  WeatherToday
+//
+//  Created by sangheon on 2020/09/30.
+//
+
+import Foundation
---
+++ WeatherToday/WeatherToday/ThirdView.swift
@@ -0,0 +1,8 @@
+//
+//  ThirdView.swift
+//  WeatherToday
+//
+//  Created by sangheon on 2020/09/30.
+//
+
+import Foundation
