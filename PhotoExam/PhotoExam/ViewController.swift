9d1d9e54019e00721f955a3e073e27d4af67724b
sangheonlee <sanghun0724@naver.com>
2020-10-10 00:01:46 +0900

    good

---
+++ PhotoExam/PhotoExam/ViewController.swift
@@ -0,0 +1,77 @@
+//
+//  ViewController.swift
+//  PhotoExam
+//
+//  Created by sangheon on 2020/10/09.
+//
+
+import UIKit
+import Photos
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    var fetchResult: PHFetchResult<PHAsset>!
+    let imageManager: PHCachingImageManager = PHCachingImageManager()
+    let cellidentifier: String = "cell"
+    
+    func requestCollection() {
+        
+        let cameraRoll: PHFetchResult<PHAssetCollection> = PHAssetCollection.fetchAssetCollections(with: .smartAlbum, subtype: .smartAlbumUserLibrary, options: nil)
+        
+        guard let cameraRollCollection = cameraRoll.firstObject else{
+            return
+        }
+        let fetchOption = PHFetchOptions()
+        fetchOption.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: false)]
+        self.fetchResult = PHAsset.fetchAssets(in: cameraRollCollection, options: fetchOption)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let photoAuthorizationStatus = PHPhotoLibrary.authorizationStatus()
+        
+        switch photoAuthorizationStatus {
+        case .authorized:
+            print("접근 허가됨")
+            self.requestCollection()
+            OperationQueue.main.addOperation {
+                self.tableView.reloadData()
+            }
+        case .denied:
+            print("접근 불허")
+        case .notDetermined:
+            print("아직 응답하지 않음")
+            PHPhotoLibrary.requestAuthorization({ (status)  in
+                switch status {
+                case.authorized:
+                    print("사용자가 허용함")
+                    self.requestCollection()
+                    self.tableView.reloadData()
+                case .denied:
+                    print("사용자가 불허함")
+                default: break
+                }
+        })
+        case . restricted:
+            print("접근 제한")
+        case .limited:
+            print("dd")
+        @unknown default:
+            print("dd")
+        }
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.fetchResult.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell:UITableViewCell = tableView.dequeueReusableCell(withIdentifier: self.cellidentifier, for: indexPath)
+        let assset:PHAsset = fetchResult.object(at: indexPath.row)
+        
+        imageManager.requestImage(for: assset, targetSize: CGSize(width: 30, height: 30), contentMode: .aspectFill, options: nil, resultHandler: {image,_ in cell.imageView?.image = image})
+        return cell
+    }
+
+}
+
