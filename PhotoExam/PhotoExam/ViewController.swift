2a08fbe1df9db9f54ed9ee53a098db8b23384d28
sangheonlee <sanghun0724@naver.com>
2020-10-10 13:13:01 +0900

    d

--- PhotoExam/PhotoExam/ViewController.swift
+++ PhotoExam/PhotoExam/ViewController.swift
@@ -7,12 +7,37 @@
 
 import UIKit
 import Photos
-class ViewController: UIViewController, UITableViewDataSource {
+class ViewController: UIViewController, UITableViewDataSource,UITableViewDelegate,PHPhotoLibraryChangeObserver {
     
     @IBOutlet weak var tableView: UITableView!
     var fetchResult: PHFetchResult<PHAsset>!
     let imageManager: PHCachingImageManager = PHCachingImageManager()
     let cellidentifier: String = "cell"
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete{
+        let asset: PHAsset = self.fetchResult[indexPath.row]
+        
+            PHPhotoLibrary.shared().performChanges({PHAssetChangeRequest.deleteAssets([asset] as NSArray)}, completionHandler: nil)
+        }
+    }
+    func photoLibraryDidChange(_ changeInstance: PHChange) {
+        guard let changes = changeInstance.changeDetails(for: fetchResult) else {
+            return
+        }
+        fetchResult = changes.fetchResultAfterChanges
+        
+        OperationQueue.main.addOperation {
+            self.tableView.reloadSections(IndexSet(0...0), with: .automatic)
+        }
+    }
+    
+    
+    
     
     func requestCollection() {
         
