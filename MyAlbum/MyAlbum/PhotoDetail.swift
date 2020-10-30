0497fa5978e5e20d6771599e82d8d2a3364614f4
sangheonlee <sanghun0724@naver.com>
2020-10-30 23:45:09 +0900

    good

---
+++ MyAlbum/MyAlbum/PhotoDetail.swift
@@ -0,0 +1,21 @@
+//
+//  PhotoDetail.swift
+//  MyAlbum
+//
+//  Created by sangheon on 2020/10/30.
+//
+
+import UIKit
+
+class PhotoDetail: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+
+}
