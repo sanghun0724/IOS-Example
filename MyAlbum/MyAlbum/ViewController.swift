4613a4b02071239294f3583e71c57442f221a922
sangheonlee <sanghun0724@naver.com>
2020-10-30 22:53:26 +0900

    리팩토링

--- MyAlbum/MyAlbum/ViewController.swift
+++ MyAlbum/MyAlbum/ViewController.swift
@@ -8,26 +8,19 @@
 import UIKit
 import Photos
 
-class ViewController: UIViewController,UICollectionViewDelegate,UICollectionViewDataSource,UINavigationControllerDelegate{
+class ViewController: UIViewController,UINavigationControllerDelegate{
     
-//    func photoLibraryDidChange(_ changeInstance: PHChange) {
-//        <#code#>
-//    }
-    
-    
-    @IBOutlet var collectionView:UICollectionView!
-    var fetchResult:[PHFetchResult<PHAsset>] = []
-    let imageManager:PHCachingImageManager = PHCachingImageManager()
-    let nextCellIdentifier:String = "photoList"
-    var selecetedcellIndex:Int = 0
-    var albumNameList:[String] = []
-    var albumCountList:[Int] = []
 
-   
-    
+    let collectionViewDatasource = CollectionViewDatasource()
+    @IBOutlet var collectionView:UICollectionView!
+
+//    let nextCellIdentifier:String = "photoList"
+//    var selecetedcellIndex:Int = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         print("sssssss")
+        collectionView.dataSource = collectionViewDatasource
+        collectionView.delegate = collectionViewDatasource
         requestPhotosPermission()
         UIdesine()
         collectionView.reloadData()
@@ -39,7 +32,7 @@ class ViewController: UIViewController,UICollectionViewDelegate,UICollectionView
         switch photoAuthorizationStatusStatus {
         case .authorized:
             print("승인되었습니다")
-            self.requestCollection()
+            self.collectionViewDatasource.requestCollection()
         case .denied:
             print("거절되었습니다")
         case .notDetermined:
@@ -49,7 +42,7 @@ class ViewController: UIViewController,UICollectionViewDelegate,UICollectionView
                 switch status {
                 case .authorized:
                     print("승인되었습니다")
-                    self.requestCollection()
+                    self.collectionViewDatasource.requestCollection()
                 case .denied:
                     print("거절되었습니다")
                     break
@@ -63,74 +56,11 @@ class ViewController: UIViewController,UICollectionViewDelegate,UICollectionView
             break
         }
     }
+   
     
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return fetchResult.count
-    }
-    
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell:ImageCollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
-                as? ImageCollectionViewCell else {
-            return UICollectionViewCell()
-        }
-        
-        let assetResult:PHAsset = fetchResult[indexPath.item].object(at: 0)
-        
-        cell.albumName.text = self.albumNameList[indexPath.item]
-        cell.albumCountTitle.text = String(self.albumCountList[indexPath.item])
-        OperationQueue.main.addOperation
-         {
-            self.imageManager.requestImage(for: assetResult, targetSize: CGSize(width: 200, height: 200), contentMode: .aspectFill, options: nil, resultHandler:{ assetResult, _ in cell.imageView?.image = assetResult   })
-        }
-       
-//        OperationQueue().addOperation {
-//            self.imageManager.requestImage(for: assetResult, targetSize: CGSize(width: 200, height: 200), contentMode: .aspectFill, options: nil, resultHandler: {
-//                assetResult, _ in  OperationQueue.main.addOperation {
-//                    cell.imageView.image = assetResult
-//                    cell.albumName.text = self.albumNameList[indexPath.item]
-//                    cell.albumCountTitle.text = String(self.albumCountList[indexPath.item])
-             
-        return cell
-        }
-//            })
-//        }
-//        return cell
-//    }
-    
-    func requestCollection(){
-        
-//        self.fetchResult.removeAll()
-//        self.albumNameList.removeAll()
-//        self.albumCountList.removeAll()
-        //self.albumCollectionList.removeAll()
-        let assetCollection: PHFetchResult<PHAssetCollection> = PHAssetCollection.fetchAssetCollections(with: .smartAlbum, subtype: .smartAlbumUserLibrary, options: nil)
-        guard let albumCollection = assetCollection.firstObject else {
-            return
-        }
-        
-        let fetchOptions = PHFetchOptions()
-        fetchOptions.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: false)]
-        let listFetchOptions = PHFetchOptions()
-        listFetchOptions.sortDescriptors = [NSSortDescriptor(key: "localizedTitle", ascending: false)]
-        let albumList: PHFetchResult<PHAssetCollection> = PHAssetCollection.fetchAssetCollections(with: .album, subtype: .albumRegular, options: listFetchOptions)
-        let albumCount = albumList.count
-        let album:[PHAssetCollection] = albumList.objects(at: IndexSet(0..<albumCount))
-        
-        fetchResult.append(PHAsset.fetchAssets(in: albumCollection, options: fetchOptions))
-        albumNameList.append("Camera Roll")
-        albumCountList.append(fetchResult[0].count)
-        
-        for i in  0..<albumCount {
-            fetchResult.append(PHAsset.fetchAssets(in: album[i], options: fetchOptions))
-            albumCountList.append(fetchResult[i+1].count)
-            albumNameList.append(album[i].localizedTitle!)
-        }
-        }
     
     func UIdesine() {
-        collectionView.dataSource = self
-        collectionView.delegate = self
+       
         
         let collectionViewLayout:UICollectionViewFlowLayout = {
             let layout = UICollectionViewFlowLayout()
