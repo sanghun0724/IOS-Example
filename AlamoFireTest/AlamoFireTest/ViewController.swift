f1cf87116a1b691e6899a43035d4fd8a4fa4af0e
sanghun0724 <sanghun0724@naver.com>
2020-11-19 22:35:59 +0900

    searchBar

--- AlamoFireTest/AlamoFireTest/ViewController.swift
+++ AlamoFireTest/AlamoFireTest/ViewController.swift
@@ -6,14 +6,43 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+
+    @IBAction func onBtnRequest(_ sender: Any) {
+        
+    }
+    
+    func sendRequest() {
+        //post 방식
+        let parameter :Parameters = [
+            "user_id" : "hong",
+            "user_pw" : "1234"
+        ]
+        let url:String = "http://nissisoft21.dothome.co.kr/login_test.php"
+        
+        
+        Alamofire.request(url,
+                          method: .post,
+                          parameter: parameter,
+                          encoding: URLEncoding.httpBody,
+                          headers: [
+                          
+                          ]
+        )
+                          
+    }
+    
 
 }
 
---
+++ SearchBarwithTableView/SearchBarwithTableView/ViewController.swift
@@ -0,0 +1,84 @@
+//
+//  ViewController.swift
+//  SearchBarwithTableView
+//
+//  Created by sangheon on 2020/11/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource,UITextFieldDelegate {
+    
+    @IBOutlet var table:UITableView!
+    @IBOutlet var field:UITextField!
+    
+    var data = [String]()
+    var filterData = [String]()
+    var filtered = false
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        setupData()
+        table.delegate = self
+        table.dataSource = self
+        field.delegate = self
+    }
+    
+    private func setupData() {
+        data.append("John")
+        data.append("Abe")
+        data.append("Dan")
+        data.append("zach")
+        data.append("Melanie")
+        data.append("Sarah")
+        data.append("Shawn")
+        data.append("Tom")
+        data.append("Teri")
+        data.append("Kevin")
+        data.append("John")
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        if let text = textField.text { //textfiled.text 가 옵셔널이라서
+            filterText(text+string)
+        }
+        return true
+    }
+    
+  
+    func filterText(_ query:String) {
+        filterData.removeAll()
+        for string in data {
+            if string.lowercased().starts(with: query) {
+                filterData.append(string)
+            }
+        }
+        table.reloadData()
+        filtered = true
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if !filterData.isEmpty {
+            return filterData.count
+        }
+        return filtered ? 0 : data.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = table.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = data[indexPath.row]
+        if !filterData.isEmpty {
+            cell.textLabel?.text = filterData[indexPath.row]
+        }
+        else {
+            cell.textLabel?.text = data[indexPath.row]
+        }
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+}
+
